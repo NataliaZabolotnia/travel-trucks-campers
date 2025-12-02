2c34e3ce845579856153d19117bdc4e585bcc3a0
Natalia Zabolotnia <zanatav78@gmail.com>
2025-12-02 22:01:51 +0200

    add svg img css header hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import css from './page.module.css';
+
 export default function TravelTrucks() {
   return (
     <main>
-      <h1>Travel-trucks</h1>
+      <section className={css.hero}>
+        <div className={css.pageContent}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.descr}>
+            You can find everything you want in our catalog
+          </p>
+          <button type="button" className={css.btnViewNow}>
+            View Now
+          </button>
+        </div>
+        {/* <Image
+          src="/images/picture.jpg"
+          alt="Hero Image"
+          width={1440}
+          height={896}
+          className={css.hero_image}
+        /> */}
+      </section>
     </main>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link';
+import css from '@/components/Header/Header.module.css';
 
 export default function Header() {
   return (
-    <div>
-      <Link href="/" aria-label="Home">
-        Home
-      </Link>
-      <Link href="/catalog" aria-label="Catalog">
-        Catalog
-      </Link>
+    <div className={css.headerContent}>
+      <svg className={css.icon} width="136" height="16" aria-hidden="true">
+        <use href="/images/icons.svg#icon-Logo"></use>
+      </svg>
+      <nav className={css.headerNav}>
+        <Link href="/" aria-label="Home">
+          Home
+        </Link>
+        <Link href="/catalog" aria-label="Catalog">
+          Catalog
+        </Link>
+      </nav>
     </div>
   );
 }
